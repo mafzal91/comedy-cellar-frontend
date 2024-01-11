6bc66122e549b5fa9abcd3fddd3bf3ca5dc87df2
Mohammad Afzal <mafzal91@gmail.com>
2024-01-11 04:05:18 -0500

    Added Env variables

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
+const { VITE_API_URL } = import.meta.env;
+
 export const fetchShowDetails = async ({ date }: { date: string }) => {
-  const res = await fetch(
-    `https://ua7yysa65h.execute-api.us-east-1.amazonaws.com/api/details?date=${date}`
-  );
+  const res = await fetch(`${VITE_API_URL}/api/details?date=${date}`);
 
   const response = await res.json();
 
