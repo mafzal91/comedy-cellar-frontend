f8a6cd5e072caf25e9c6b5c86f1052aa7d162a7e
Mohammad Afzal <mohammadafzal@Mohammads-Mac-mini.local>
2024-01-25 01:01:21 -0500

    Added sentry with basic error tracking

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import { render } from "preact";
+import * as Sentry from "@sentry/browser";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { LocationProvider } from "preact-iso";
 
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home/index.jsx";
 import "./style.css";
+
+Sentry.init({
+  dsn: "https://523bcd0e95c565ef1f4c580690e75a9b@o4506630100090880.ingest.sentry.io/4506630117916672",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  enabled: import.meta.env.PROD,
+});
 
 const queryClient = new QueryClient();
 
